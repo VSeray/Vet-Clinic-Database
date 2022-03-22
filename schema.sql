2ba21b73fdab983dbe8c19e88f0dd8594f71bcc4
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-03-22 15:58:16 +0200

    Create new tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,31 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(20);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name varchar(20),
+    age int
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(20)
+);
+
+--MODIFY ANIMALS TABLE
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+    ADD species_id int,
+    ADD CONSTRAINT species_id FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owners_id int, ADD CONSTRAINTS owners_id FOREIGN KEY(owners_id) REFERENCES owners(id);
