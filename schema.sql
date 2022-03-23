c7296d180b667de8079d6007bd4d58b49ba951f2
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-03-23 10:18:52 +0200

    Update files add "join table" for visit

--- schema.sql
+++ schema.sql
@@ -40,3 +40,27 @@ ALTER TABLE animals
     ADD CONSTRAINT species_id FOREIGN KEY(species_id) REFERENCES species(id);
 
 ALTER TABLE animals ADD owners_id int, ADD CONSTRAINT owners_id FOREIGN KEY(owners_id) REFERENCES owners(id);
+
+----
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name varchar(100),
+    age int,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+    species_id int,
+    vets_id int,
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animals_id int,
+    vets_id int,
+    date_of_visit date,
+    CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
