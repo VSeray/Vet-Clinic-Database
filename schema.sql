e5cfa9b2a0eb0c8b5199c2730476a5a6e0dfea68
mrparkersson <amoparkerprince78@gmail.com>
2022-03-28 10:29:58 +0000

    Add indexes to schemal file

--- schema.sql
+++ schema.sql
@@ -68,4 +68,8 @@ CREATE TABLE visits (
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
+CREATE INDEX visits_animals_id  ON visits(animals_id);
+CREATE INDEX visits_vets_id  ON visits(vets_id);
+CREATE INDEX owners_id  ON owners(email DESC);
+
 
